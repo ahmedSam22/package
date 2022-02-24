3bc9cdec9a456f4c138e996949125ad910958cea
ahmedSam22 <58396761+ahmedSam22@users.noreply.github.com>
2022-02-24 14:31:47 +0200

    not yet

--- main.js
+++ main.js
@@ -3,16 +3,17 @@ const path = require("path");
 const fs = require("fs");
 const createSpinner = require("nanospinner");
 
-function install(str) {
-    return new Promise((resolve , reject)=>{
-        exec(str).on("close" , (code)=>{
-            if(code ===0){
-                resolve();
-            }
-            reject()
-        })
-    })
-}
+
+// function install(str) {
+//     return new Promise((resolve , reject)=>{
+//         exec(str).on("close" , (code)=>{
+//             if(code ===0){
+//                 resolve();
+//             }
+//             reject()
+//         })
+//     })
+// }
 
 if(process.argv.length < 3){
     console.log("enter a name please");
