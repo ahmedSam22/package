9978dfaaf44ac4ebf97f0bd39dba2d19d452d149
ahmedSam22 <58396761+ahmedSam22@users.noreply.github.com>
2022-02-24 22:52:16 +0200

    test

--- main.js
+++ main.js
@@ -4,18 +4,34 @@ const fs = require("fs");
 const createSpinner = require("nanospinner");
 
 
-// function install(str) {
-//     return new Promise((resolve , reject)=>{
-//         exec(str).on("close" , (code)=>{
-//             if(code ===0){
-//                 resolve();
-//             }
-//             reject()
-//         })
-//     })
-// }
+function install(str) {
+    return new Promise((resolve , reject)=>{
+        exec(str).on("close" , (code)=>{
+            if(code ===0){
+                resolve();
+            }
+            reject()
+        })
+    })
+}
 
 if(process.argv.length < 3){
     console.log("enter a name please");
     process.exit(1);
+}
+
+const projectName = process.argv[2];
+currentPath = process.cwd();
+const projectPath = path.join(currentPath , projectName);
+const repo = "https://github.com/ahmedSam22/angproject.git";
+
+try {
+    fs.mkdirSync(projectPath);
+} catch (err) {
+    if (err.code === "EEXIST"){
+        console.log(`this file ${projectName} mawgoud`);
+    }else{
+        console.log(error);
+    }
+    process.exit(1)
 }
