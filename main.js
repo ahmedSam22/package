3d4a11eaee0776f4237cdbe3361faa03bf5888e2
Ahmed samir <tahmsamir288@tutamail.com>
2022-06-11 17:54:06 +0200

    update

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ async function main() {
           "installing node packages... \n"
         ).start();
 
-        path.dirname(projectName);
+        path.dirname(`./${projectName}`);
         install(`npm i`).then((_) => {
             spinner3.success({ text: "Every thing is okay Now  !" });
           });
