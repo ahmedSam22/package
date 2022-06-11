a736ab5f0c361e9d7cd72d24f0b2cea39d0704b3
Ahmed samir <tahmsamir288@tutamail.com>
2022-06-11 16:58:10 +0200

    update

--- main.js
+++ main.js
@@ -3,64 +3,61 @@
 const { execSync, exec } = require("child_process");
 const path = require("path");
 const fs = require("fs");
-const {createSpinner} = require("nanospinner");
-
+const { createSpinner } = require("nanospinner");
 
 function install(str) {
-    return new Promise((resolve, reject) => {
-        exec(str).on("close", (code) => {
-            if (code === 0) {
-                resolve();
-            }
-            reject()
-        })
-    })
+  return new Promise((resolve, reject) => {
+    exec(str).on("close", (code) => {
+      if (code === 0) {
+        resolve();
+      }
+      reject();
+    });
+  });
 }
 
 if (process.argv.length < 3) {
-    console.log("enter a name please");
-    process.exit(1);
+  console.log("enter a name please");
+  process.exit(1);
 }
 
 const projectName = process.argv[2];
 currentPath = process.cwd();
+const currPath = "./angproject";
 const projectPath = path.join(currentPath, projectName);
 const repo = "https://github.com/ahmedSam22/angproject.git";
 
 try {
-    fs.mkdirSync(projectPath);
+  fs.mkdirSync(projectPath);
 } catch (err) {
-    if (err.code === "EEXIST") {
-        console.log(`this file ${projectName} mawgoud`);
-    } else {
-        console.log(error);
-    }
-    process.exit(1)
-} 
+  if (err.code === "EEXIST") {
+    console.log(`this file ${projectName} mawgoud`);
+  } else {
+    console.log(error);
+  }
+  process.exit(1);
+}
 async function main() {
-
-    try{
+  try {
     const spinner = createSpinner("Downloadingggggggggg files ... \n").start();
-    // process.chdir(projectName);
+    process.chdir(projectName);
     await install(`git clone ${repo}`);
     // process.chdir("./angproject/server");
-    
-    spinner.success({ text: 'file downloaded' });
-    // const spinner2 = createSpinner("installing dep ..\n").start();
-    // await install("npm i");
-    // spinner2.success({ text: "done" });
-    // process.chdir("../");
-    // const spinner3 = createSpinner("install client ..\n").start();
 
-    // await install("npm i");
+    const spinner2 = createSpinner("set Project Name ... \n").start();
 
-    // spinner3.success({ text: "donee" })
-    // process.chdir("../");
-    // await install("code .");
-    // console.log("doneeeeeeeeeeeeeee")
-// return ;
-    } catch (error) {
+    fs.rename(currPath, projectName, function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        spinner2.success({ text: "file downloaded" });
+      }
+    });
+
+    spinner.success({ text: "file downloaded" });
+  } catch (error) {
     console.log(error);
-}}
+  }
+}
 
 main();
