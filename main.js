242a11a8314ce4200322e146d864d0121230d96e
Ahmed samir <tahmsamir288@tutamail.com>
2022-06-11 18:17:14 +0200

    update

--- main.js
+++ main.js
@@ -60,13 +60,11 @@ async function main() {
 
         process.chdir(`./${projectName}`);
         install(`npm i`).then((_) => {
-            spinner3.success({ text: "Every thing is okay Now  !" });
-          }).then(_=>{
-              spinner.success({ text: "now you can start . Good Luck :)" });
-            
+            spinner3.success({ text: "now you can start . Good Luck :)" });
           });
     
 
+    spinner.success({ text: "now you can start . Good Luck :)" });
   } catch (error) {
     console.log(error);
   }
